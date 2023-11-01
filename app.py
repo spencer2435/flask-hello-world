994c28c5a3d92271190e125ba78f53d11e3233eb
Spencer Oram <spor3370@colorado.edu>
2023-11-01 19:31:52 +0000

    db_create route

--- app.py
+++ app.py
@@ -13,3 +13,19 @@ def db_test():
     conn.close()
     return "Database Connection Successful"
     
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgres://cloud_db_r72t_user:uTywBKnB4zu0zirzaSnmlNdqiG5TWGZa@dpg-cl19vk3mgg9c738bcrng-a/cloud_db_r72t")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
