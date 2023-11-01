8f523ecc5dad6abd7e869facd1a9665158bad64d
Spencer Oram <spor3370@colorado.edu>
2023-11-01 19:04:12 +0000

    db_test route

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgres://cloud_db_r72t_user:uTywBKnB4zu0zirzaSnmlNdqiG5TWGZa@dpg-cl19vk3mgg9c738bcrng-a/cloud_db_r72t")
+    conn.close()
+    return "Database Connection Successful"
+    
